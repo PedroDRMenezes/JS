2476c833d346242b2e20dd252b036ef758e2f461
danisacramento <PedroDRMenezes@gmail.com>
2019-11-28 15:50:44 -0300

    Jogo da Velha

---
+++ Jogo da Velha/xadres.js
@@ -0,0 +1,124 @@
+var jogador = 1;
+
+function Escreve1() {
+    if (document.getElementById("1").innerHTML == "") {
+        if (jogador == 1) {
+            document.getElementById("1").innerHTML = "X"
+            jogador++
+        }
+        else {
+            document.getElementById("1").innerHTML = "O"
+            jogador--
+        }
+    }
+}
+
+function Escreve2() {
+    if (document.getElementById("2").innerHTML == "") {
+        if (jogador == 1) {
+            document.getElementById("2").innerHTML = "X"
+            jogador++
+        }
+        else {
+            document.getElementById("2").innerHTML = "O"
+            jogador--
+        }
+    }
+}
+
+function Escreve3() {
+    if (document.getElementById("3").innerHTML == "") {
+        if (jogador == 1) {
+            document.getElementById("3").innerHTML = "X"
+            jogador++
+        }
+        else {
+            document.getElementById("3").innerHTML = "O"
+            jogador--
+        }
+    }
+}
+
+function Escreve4() {
+    if (document.getElementById("4").innerHTML == "") {
+        if (jogador == 1) {
+            document.getElementById("4").innerHTML = "X"
+            jogador++
+        }
+        else {
+            document.getElementById("4").innerHTML = "O"
+            jogador--
+        }
+    }
+}
+
+function Escreve5() {
+    if (document.getElementById("5").innerHTML == "") {
+        if (jogador == 1) {
+            document.getElementById("5").innerHTML = "X"
+            jogador++
+        }
+        else {
+            document.getElementById("5").innerHTML = "O"
+            jogador--
+        }
+    }
+}
+
+function Escreve6() {
+    if (document.getElementById("6").innerHTML == "") {
+        if (jogador == 1) {
+            document.getElementById("6").innerHTML = "X"
+            jogador++
+        }
+        else {
+            document.getElementById("6").innerHTML == "O"
+            jogador--
+        }
+    }
+}
+
+function Escreve7() {
+    if (document.getElementById("7").innerHTML == "") {
+        if (jogador == 1) {
+            document.getElementById("7").innerHTML = "X"
+            jogador++
+        }
+        else {
+            document.getElementById("7").innerHTML = "O"
+            jogador--
+        }
+    }
+}
+
+function Escreve8() {
+    if (document.getElementById("8").innerHTML == "") {
+        if (jogador == 1) {
+            document.getElementById("8").innerHTML = "X"
+            jogador++
+        }
+        else {
+            document.getElementById("8").innerHTML = "O"
+            jogador--
+        }
+    }
+}
+
+function Escreve9() {
+    if (document.getElementById("9").innerHTML == "") {
+
+        if (jogador == 1) {
+            document.getElementById("9").innerHTML = "X"
+            jogador++
+        }
+        else {
+            document.getElementById("9").innerHTML = "O"
+            jogador--
+        }
+    }
+}
+
+
+
+
+
