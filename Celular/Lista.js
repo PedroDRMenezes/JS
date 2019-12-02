ba5d667390b8929e9f46a41eefdaeecd917c1b7a
lumccds <PedroDRMenezes@gmail.com>
2019-12-02 15:51:28 -0300

    02/12/19

--- Celular/Lista.js
+++ Celular/Lista.js
@@ -1,16 +1,16 @@
 var I = 1;
 function Botao() {
     var texto = document.getElementById("tarefa").value;
-    document.getElementById("resposta").innerHTML += "<div class='col-md-2'><input type='checkbox' aria-label='Chebox para permitir input text' onclick='check();'></div><div class='col-md-8' id='corte'>"+texto+"</div><div class='col-md-2'>"+I+"</div>"
+    document.getElementById("resposta").innerHTML += "<div class='col-md-2'><input type='checkbox' aria-label='Chebox para permitir input text' onclick='check(this," + I + ");'></div><div class='col-md-8' id='corte" + I + "'>" + texto + "</div><div class='col-md-2'>" + I + "</div>"
     I++
 }
-
-function check() {
-    var texto2 = document.getElementById("corte").value;
-    document.getElementById("resposta").innerHTML="<stryke>"+texto2+"</stryke>" 
-
+var texto2 = "";
+function check(ch,id) {
+    if (ch.checked) {
+        document.getElementById("corte" + id).style.textDecoration = "line-through";
+    }
+    else{
+        document.getElementById("corte" + id).style.textDecoration = "";
+    }
 }
 
-//"<div class='col-md-2'><input type='radio' aria-label='Botão radio para acompanhar input text'></div><div class='col-md-8'>" + texto + "</div><div class='col-md-2'>" + I + "</div>"
-
-//<div class='col-md-2'><input type='checkbox' aria-label='Chebox para permitir input text' onclick='check();'></div><div class='col-md-8' id='corte'>"texto"</div><div class='col-md-2'>"I"</div>
